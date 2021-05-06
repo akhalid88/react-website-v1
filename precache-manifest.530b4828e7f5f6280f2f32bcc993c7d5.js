48d9b9aa4c11396f124c096f4584549cb31170eb
Muhammad K <akhalid.code@gmail.com>
2021-05-06 14:33:00 -0700

    Updates

---
+++ precache-manifest.530b4828e7f5f6280f2f32bcc993c7d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de7831df714a76ff6da1a1bb40c6e59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e04728483824e21b0ddd",
+    "url": "/static/css/main.b3209779.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04728483824e21b0ddd",
+    "url": "/static/js/main.58aa6b7a.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
